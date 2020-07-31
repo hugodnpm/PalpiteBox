7a17405f0ae202f51bee5c0f7012239be46bad44
hugodnpm <hugosc@gmail.com>
2020-07-31 17:41:01 -0300

    fix connect sheet to vercel

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,17 +1,15 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
+import { fromBase64 } from '../../utils/base64'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
-const fromBase64 = value => {
-    const buff = Buffer.from(value, 'base64');
-    return buff.toString('ascii');
-}
+
 
 export default async (req, res) => {
     try{
         await doc.useServiceAccountAuth({
             client_email: process.env.SHEET_CLIENT_EMAIL,
-            private_key: process.env.SHEET_PRIVATE_KEY
+            private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
         })
         await doc.loadInfo()
         
